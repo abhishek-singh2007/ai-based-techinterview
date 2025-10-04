56da051540d41dfe59eb3d1de5710cdf4eb97855
abhishek-singh2007 <abhishekknpsingh7@gmail.com>
2025-10-04 14:44:38 +0530

    Added feedback page, generate.action.ts, updated env setup

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,16 +1,17 @@
 import Agent from "@/components/Agent";
-
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const Page = async () => {
-
+    const user = await getCurrentUser();
 
     return (
         <>
             <h3>Interview generation</h3>
 
             <Agent
-                userName="your"
-                userId="user1"
+                userName={user?.name!}
+                userId={user?.id}
+                profileImage={user?.profileURL}
                 type="generate"
             />
         </>
